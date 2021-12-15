51e46b44fb652a1e279489736414a0f37a9066ec
AntonioFBove <antoniofbove@gmail.com>
2021-12-15 15:35:01 +0100

    snack 4

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,1 +1,14 @@
-let num
+const desiredLength = 4;    // in modo da poter cambiare successivamente il numero di cifre
+
+let numberString = '';
+let sum = 0;
+
+do {
+  numberString = prompt(`Inserisci un numero a ${desiredLength} cifre`);
+} while (numberString.length !== desiredLength);
+
+for (let i = 0; i < numberString.length; i++) {
+  sum += parseInt(numberString[i]);
+}
+
+console.log(sum);
